4e8c9a1aa811b24534fbb0582b4300fc08a09c80
Lindsey Baker <lindsey.baker0@gmail.com>
2019-05-03 16:04:33 -0700

    Add delete function, reformat Index

--- WordCounter/Controllers/CounterController.cs
+++ WordCounter/Controllers/CounterController.cs
@@ -7,25 +7,32 @@ namespace WordCounter.Controllers
   public class CounterController : Controller
   {
     [HttpGet("/counter")]
-   public ActionResult Index()
-   {
+    public ActionResult Index()
+    {
 
-     List<Counter> allCounters = Counter.UserInput;
-     return View(allCounters);
-   }
+      List<Counter> allCounters = Counter.UserInput;
+      return View(allCounters);
+    }
 
     [HttpGet("/counter/new")]
-  public ActionResult New()
-  {
-    return View();
-  }
+    public ActionResult New()
+    {
+      return View();
+    }
 
-  [HttpPost("/counter")]
-  public ActionResult Create(string wordToCount, string sentenceToCheck)
-  {
-    Counter myCounter = new Counter(wordToCount, sentenceToCheck);
-    return RedirectToAction("Index");
-  }
+    [HttpPost("/counter")]
+    public ActionResult Create(string wordToCount, string sentenceToCheck)
+    {
+      Counter myCounter = new Counter(wordToCount, sentenceToCheck);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/counter/{id}/delete")]
+    public ActionResult Destroy(int id)
+    {
+      Counter.RemoveCounter(id);
+      return RedirectToAction("Index");
+    }
 
   }
 }
